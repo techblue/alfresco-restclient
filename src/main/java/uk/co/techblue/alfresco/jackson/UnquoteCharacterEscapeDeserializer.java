8b3fc3509a303ef606e51d9fdd96479045199233
Dishant Anand <dishant.anand@techblue.co.uk>
2014-08-23 15:50:18 +0100

    code formatting.

--- src/main/java/uk/co/techblue/alfresco/jackson/UnquoteCharacterEscapeDeserializer.java
+++ src/main/java/uk/co/techblue/alfresco/jackson/UnquoteCharacterEscapeDeserializer.java
@@ -1,6 +1,17 @@
 /*******************************************************************************
- * Copyright 2013, Barbon. All Rights Reserved. 
- * No part of this content may be used without Barbon's express consent.
+ * Copyright 2012 Technology Blueprint Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 package uk.co.techblue.alfresco.jackson;
 
@@ -17,7 +28,6 @@ import org.codehaus.jackson.type.TypeReference;
 /**
  * The Class StringEscapeDeserializer.
  * 
- * @author <a href="mailto:dishant.anand@techblue.co.uk">Dishant anand</a>
  */
 public class UnquoteCharacterEscapeDeserializer extends JsonDeserializer<Map<String, String>> {
 
